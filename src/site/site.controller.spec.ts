99c20f0184e37c01db946c0b4ce10a7e57c19a86
Jake <jake.zwirdowski@gmail.com>
2024-01-29 21:36:36 +0100

    Added controller and service tests

--- src/site/site.controller.spec.ts
+++ src/site/site.controller.spec.ts
@@ -1,20 +1,58 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { SiteController } from './site.controller'
 import { SiteService } from './site.service'
+import { Site } from './entities/site.entity'
+import { ObjectId } from 'mongodb'
+import { getRepositoryToken } from '@nestjs/typeorm'
 
 describe('SiteController', () => {
   let controller: SiteController
+  let service: SiteService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [SiteController],
-      providers: [SiteService],
+      providers: [
+        SiteService,
+        {
+          provide: getRepositoryToken(Site),
+          useValue: {}, // the shape of the mocked repository shouldn't matter as in controller test
+          // the values of the mocked service (with spyOn()) should be returned
+          // irrelevant of the mocked repository
+        },
+      ],
     }).compile()
 
     controller = module.get<SiteController>(SiteController)
+    service = module.get<SiteService>(SiteService)
   })
 
   it('should be defined', () => {
     expect(controller).toBeDefined()
+  })
+
+  describe('GET', () => {
+    it('should return sites returned by the service', async () => {
+      const site1 = new Site()
+      site1._id = new ObjectId('123456789012345678901234')
+      site1.name = 'site 1'
+      site1.x = 1
+
+      const site2 = new Site()
+      site2._id = new ObjectId('123456789012345678901235')
+      site2.name = 'site 2'
+      site2.x = 2
+
+      const site3 = new Site()
+      site3._id = new ObjectId('123456789012345678901236')
+      site3.name = 'site 3'
+      site3.x = 3
+
+      const sites = [site1, site2, site3]
+
+      jest.spyOn(service, 'findAll').mockImplementation(async () => sites)
+
+      expect(await controller.findAll()).toHaveLength(3)
+    })
   })
 })
--- src/site/site.service.spec.ts
+++ src/site/site.service.spec.ts
@@ -1,18 +1,57 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { SiteService } from './site.service'
+import { getRepositoryToken } from '@nestjs/typeorm'
+import { Site } from './entities/site.entity'
+import { ObjectId } from 'mongodb'
 
 describe('SiteService', () => {
-  let service: SiteService
+  // let service: SiteService
 
-  beforeEach(async () => {
+  // beforeEach(async () => {
+  // })
+
+  async function getMockedService(repositoryMock: object) {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [SiteService],
+      providers: [
+        SiteService,
+        {
+          provide: getRepositoryToken(Site),
+          useValue: repositoryMock,
+        },
+      ],
     }).compile()
 
-    service = module.get<SiteService>(SiteService)
-  })
+    return module.get<SiteService>(SiteService)
+  }
 
-  it('should be defined', () => {
-    expect(service).toBeDefined()
+  // it('should be defined', () => {
+  //   expect(service).toBeDefined()
+  // })
+
+  describe('findAll', () => {
+    it('should return sites returned by the repository', async () => {
+      const site1 = new Site()
+      site1._id = new ObjectId('123456789012345678901234')
+      site1.name = 'site 1'
+      site1.x = 1
+
+      const site2 = new Site()
+      site2._id = new ObjectId('123456789012345678901235')
+      site2.name = 'site 2'
+      site2.x = 2
+
+      const site3 = new Site()
+      site3._id = new ObjectId('123456789012345678901236')
+      site3.name = 'site 3'
+      site3.x = 3
+
+      const sites = [site1, site2, site3]
+
+      const mockedService = await getMockedService({
+        find: jest.fn(async () => sites),
+      })
+
+      expect(await mockedService.findAll()).toHaveLength(3)
+    })
   })
 })
